e092a1399e45d51ca553c1d30afafd73bea23ee8
Sofía Makita <sophiamakita@gmail.com>
2025-06-26 13:05:56 -0300

    starting cart

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { RouterLink } from 'vue-router';
 
 defineComponent({
   name: 'NavbarComp',
 })
+
+const cartDialog = ref(false);
+
+const toggleCart = () => cartDialog.value = !cartDialog.value;
 
 </script>
 
@@ -21,8 +25,26 @@ defineComponent({
       <RouterLink class="links" to="/kids" active-class="active-link">Kids</RouterLink>
     </nav>
     <v-col class="d-flex justify-end">
-      <v-icon class="ml-4">mdi-cart</v-icon>
-      <v-icon class="ml-4">mdi-account</v-icon>
+      <v-icon @click="toggleCart" class="icon ml-4">mdi-cart</v-icon>
+      <!-- <v-dialog v-model="cartDialog">
+        <v-card class="dialogCard" width="40rem">
+          hi
+        </v-card>
+      </v-dialog> -->
+      <div v-if="cartDialog" class="overlay">
+        <v-card variant="outlined" class="dialogCard" width="30rem">
+          <v-card-title class="d-flex justify-space-between">
+            Cart:
+            <p v-if="closeHover" class="mb-0">Close</p>
+            <v-icon @click="toggleCart" style="font-size: 1.5rem;">mdi-close</v-icon>
+          </v-card-title>
+          <v-card-text>fdfdf</v-card-text>
+        </v-card>
+      </div>
+
+      <RouterLink to="/profile">
+        <v-icon class="icon ml-4">mdi-account</v-icon>
+      </RouterLink>
     </v-col>
   </header>
 </template>
@@ -50,5 +72,39 @@ header {
 
 .active-link {
   font-weight: bold;
+}
+
+.icon {
+  color: black;
+}
+
+.icon:hover {
+  color: var(--mid-green);
+}
+
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  backdrop-filter: blur(3px);
+  -webkit-backdrop-filter: blur(3px);
+  background-color: rgba(0, 0, 0, 0.2);
+  /* dark tint */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: +2;
+}
+
+
+.dialogCard {
+  position: absolute;
+  min-height: 10rem;
+  z-index: +4;
+  top: 12%;
+  right: 10%;
+  background-color: white;
 }
 </style>
